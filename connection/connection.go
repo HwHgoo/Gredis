b3410d29c8424abaadb35ea0e43e71ceb76f7c66
huwenhao <huwenhao@qq.com>
2024-07-12 17:18:15 +0800

    完成简单的connection管理

--- connection/connection.go
+++ connection/connection.go
@@ -3,9 +3,19 @@ package connection
 import "net"
 
 type Connection struct {
-	conn net.Conn
+	conn       net.Conn
+	selectedDb int
 }
 
 func MakeConnection(conn net.Conn) *Connection {
-	return nil
+	return &Connection{conn: conn}
+}
+
+func (c *Connection) Write(data []byte) error {
+	_, err := c.conn.Write(data)
+	return err
+}
+
+func (c *Connection) GetSelectedDb() int {
+	return c.selectedDb
 }
