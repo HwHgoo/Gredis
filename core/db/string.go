b96ad1380908b30c398dcab312b645c1230cb332
huwenhao <Kkz.twist@gmail.com>
2024-07-18 15:44:03 +0800

    使用parseExtendedStringArgumentOrReply替代参数校验；完成get和incr相关的操作

--- core/db/string.go
+++ core/db/string.go
@@ -1,12 +1,14 @@
 package db
 
 import (
+	"math/big"
 	"strconv"
 	"strings"
 	"time"
 
 	"github.com/HwHgoo/Gredis/core/protocol"
 	"github.com/HwHgoo/Gredis/global"
+	"github.com/HwHgoo/Gredis/utils/pool"
 )
 
 func (db *Database) getAsString(key string) ([]byte, protocol.RedisErrorMessage) {
@@ -56,7 +58,7 @@ const (
  * If nil is returned, a duration value is also returned if needed.
  * The unit of duraton is always in milliseconds.
  */
-func parseExtendedStringArgumentOrReply(args [][]byte, flags *int, command_type int) (protocol.RedisMessage, time.Duration) {
+func parseExtendedStringArgumentOrReply(args [][]byte, flags *int, command_type int) (protocol.RedisErrorMessage, time.Duration) {
 	d := time.Duration(0)
 	unixms := time.Now().UnixMilli()
 	for i := 0; i < len(args); i++ {
@@ -69,7 +71,7 @@ func parseExtendedStringArgumentOrReply(args [][]byte, flags *int, command_type 
 			*flags |= flag_ex
 			ex, err := strconv.ParseInt(string(args[i+1]), 10, 64)
 			if err != nil {
-				return &protocol.InvalidNumberError, 0
+				return &protocol.InvalidIntegerError, 0
 			}
 			if ex <= 0 {
 				return &protocol.InvalidExpireTimeError, 0
@@ -84,7 +86,7 @@ func parseExtendedStringArgumentOrReply(args [][]byte, flags *int, command_type 
 			*flags |= flag_px
 			px, err := strconv.ParseInt(string(args[i+1]), 10, 64)
 			if err != nil {
-				return &protocol.InvalidNumberError, 0
+				return &protocol.InvalidIntegerError, 0
 			}
 			if px <= 0 {
 				return &protocol.InvalidExpireTimeError, 0
@@ -98,7 +100,7 @@ func parseExtendedStringArgumentOrReply(args [][]byte, flags *int, command_type 
 			*flags |= flag_exat
 			exat, err := strconv.ParseInt(string(args[i+1]), 10, 64)
 			if err != nil {
-				return &protocol.InvalidNumberError, 0
+				return &protocol.InvalidIntegerError, 0
 			}
 			if exat <= 0 {
 				return &protocol.InvalidExpireTimeError, 0
@@ -112,7 +114,7 @@ func parseExtendedStringArgumentOrReply(args [][]byte, flags *int, command_type 
 			*flags |= flag_pxat
 			pxat, err := strconv.ParseInt(string(args[i+1]), 10, 64)
 			if err != nil {
-				return &protocol.InvalidNumberError, 0
+				return &protocol.InvalidIntegerError, 0
 			}
 			if pxat <= 0 {
 				return &protocol.InvalidExpireTimeError, 0
@@ -152,6 +154,8 @@ func parseExtendedStringArgumentOrReply(args [][]byte, flags *int, command_type 
 
 	return nil, d
 }
+
+/************************************* GET ************************************/
 
 func get(db *Database, args [][]byte) protocol.RedisMessage {
 	key := string(args[0])
@@ -211,102 +215,147 @@ func getex(db *Database, args [][]byte) protocol.RedisMessage {
 	return protocol.MakeBulkString(s)
 }
 
+// GETRANGE get a substring of the string stored at key
+func getrange(db *Database, args [][]byte) protocol.RedisMessage {
+	key := string(args[0])
+	start, err := strconv.ParseInt(string(args[1]), 10, 64)
+	if err != nil {
+		return &protocol.InvalidIntegerError
+	}
+	end, err := strconv.ParseInt(string(args[2]), 10, 64)
+	if err != nil {
+		return &protocol.InvalidIntegerError
+	}
+
+	if start > end {
+		return protocol.MakeBulkString(nil)
+	}
+
+	b, err := db.getAsString(key)
+	if err != nil {
+		return protocol.MakeBulkString(b)
+	}
+
+	length := int64(len(b))
+	start = (start + length) % length
+	end = (end + length) % length
+
+	return protocol.MakeBulkString(b[start : end+1])
+}
+
+// GETSET is deprecated, use SET with GET option instead.
+
+/************************************* INCR/DECR ************************************/
+
+func incrdecrGeneric(db *Database, key string, delta int64) protocol.RedisMessage {
+	b, err := db.getAsString(key)
+	if err != nil {
+		return err
+	}
+
+	n, e := strconv.ParseInt(string(b), 10, 64)
+	if e != nil {
+		return protocol.InvalidIntegerError
+	}
+
+	n += delta
+	v := strconv.FormatInt(n, 10)
+	db.Set(key, []byte(v))
+
+	return protocol.MakeInteger(n)
+}
+
+func incr(db *Database, args [][]byte) protocol.RedisMessage {
+	key := string(args[0])
+	return incrdecrGeneric(db, key, 1)
+}
+
+func incrby(db *Database, args [][]byte) protocol.RedisMessage {
+	key := string(args[0])
+	delta, err := strconv.ParseInt(string(args[1]), 10, 64)
+	if err != nil {
+		return &protocol.InvalidIntegerError
+	}
+
+	return incrdecrGeneric(db, key, delta)
+}
+
+func decr(db *Database, args [][]byte) protocol.RedisMessage {
+	key := string(args[0])
+	return incrdecrGeneric(db, key, -1)
+}
+
+func decrby(db *Database, args [][]byte) protocol.RedisMessage {
+	key := string(args[0])
+	delta, err := strconv.ParseInt(string(args[1]), 10, 64)
+	if err != nil {
+		return &protocol.InvalidIntegerError
+	}
+	return incrdecrGeneric(db, key, -delta)
+}
+
+var bigfloats = pool.MakePool(1024, func() *big.Float {
+	f := big.NewFloat(0)
+	return f.SetPrec(80) // simulate long double
+})
+
+// NOT as sensiable as redis
+func incrbyfloat(db *Database, args [][]byte) protocol.RedisMessage {
+	key := string(args[0])
+	var err error
+	delta := bigfloats.Get()
+	defer bigfloats.Put(delta)
+	delta, _, err = delta.Parse(string(args[1]), 0)
+	if err != nil {
+		return &protocol.InvalidFloatError
+	}
+
+	f, msg := db.getAsString(key)
+	if msg != nil {
+		return msg
+	}
+	fv := bigfloats.Get()
+	defer bigfloats.Put(fv)
+	fv, _, err = fv.Parse(string(f), 0)
+	if err != nil {
+		return protocol.InvalidFloatError
+	}
+
+	fv = fv.Add(fv, delta)
+	res := strings.TrimRight(fv.Text('f', 17), "0")
+	db.Set(key, []byte(res))
+	return protocol.MakeBulkString([]byte(res))
+}
+
+/************************************* SET ************************************/
+
 func set(db *Database, args [][]byte) protocol.RedisMessage {
 	key := string(args[0])
 	value := args[1]
-	setType := upsert
-	ttl := time.Second * 0
-	flagGet := false
-	flagKeepttl := false
-
-	for i := 2; i < len(args); i++ {
-		arg := strings.ToLower(string(args[i]))
-		if arg == "nx" {
-			if setType == update || flagGet {
-				return &protocol.SyntaxError
-			}
-			setType = insert
-		} else if arg == "xx" {
-			if setType == insert {
-				return &protocol.SyntaxError
-			}
-			setType = update
-		} else if arg == "ex" {
-			if ttl != 0 {
-				return &protocol.SyntaxError
-			}
-
-			if i+1 >= len(args) {
-				return &protocol.SyntaxError
-			}
-
-			ex, err := strconv.ParseInt(string(args[i+1]), 10, 64)
-			if err != nil {
-				return &protocol.InvalidNumberError
-			}
-
-			if ex <= 0 {
-				return &protocol.InvalidExpireTimeError
-			}
-
-			ttl = time.Second * time.Duration(ex)
-			i++
-		} else if arg == "px" {
-			if ttl != 0 || i+1 >= len(args) {
-				return &protocol.SyntaxError
-			}
-
-			px, err := strconv.ParseInt(string(args[i+1]), 10, 64)
-			if err != nil {
-				return &protocol.InvalidNumberError
-			}
-
-			if px <= 0 {
-				return &protocol.InvalidExpireTimeError
-			}
-
-			ttl = time.Millisecond * time.Duration(px)
-			i++
-		} else if arg == "keepttl" {
-			if setType != upsert {
-				return &protocol.SyntaxError
-			}
-			flagKeepttl = true
-		} else if arg == "get" {
-			if setType == insert {
-				return &protocol.SyntaxError
-			}
-			flagGet = true
-		} else {
-			return &protocol.SyntaxError
-		}
-	}
-
-	var getMsg protocol.RedisMessage
-
-	if flagGet {
-		getMsg = get(db, args)
+	flag := flag_no_flag
+	err, ttl := parseExtendedStringArgumentOrReply(args[2:], &flag, command_set)
+	if err != nil {
+		return err
 	}
 
 	result := global.ERR
-	if setType == upsert {
+	if flag&flag_set_nx != 0 {
+		result = db.SetIfAbsent(key, value)
+	} else if flag&flag_set_xx != 0 {
+		result = db.SetIfExist(key, value)
+	} else {
 		db.Set(key, value)
 		result = global.OK
-	} else if setType == insert {
-		result = db.SetIfAbsent(key, value)
-	} else {
-		result = db.SetIfExist(key, value)
 	}
 
-	if ttl > 0 {
-		expireAt := time.Now().Add(ttl)
-		db.Expire(key, expireAt)
-	} else if !flagKeepttl {
+	if flag&flag_keepttl != 0 {
 		db.Persist(key)
+	} else if withFlags(flag, flag_ex, flag_px, flag_exat, flag_pxat) {
+		db.Expire(key, time.Now().Add(ttl))
 	}
 
-	if flagGet {
-		return getMsg
+	if withFlags(flag, flag_set_get) {
+		return get(db, args)
 	}
 
 	if result == global.OK {
@@ -335,6 +384,12 @@ func init() {
 	RegisterCommand("del", -2, del)
 	RegisterCommand("getdel", 2, getdel)
 	RegisterCommand("getex", -2, getex)
+	RegisterCommand("getrange", 4, getrange)
+	RegisterCommand("incr", 2, incr)
+	RegisterCommand("incrby", 3, incrby)
+	RegisterCommand("decr", 2, decr)
+	RegisterCommand("decrby", 3, decrby)
+	RegisterCommand("incrbyfloat", 3, incrbyfloat)
 }
 
 // withFlags checks if the given flag contains some of the given flags.
