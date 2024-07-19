85a6c824cc6c7b4a16e3ceeceb398e6ddc4fc2c2
huwenhao <Kkz.twist@gmail.com>
2024-07-19 20:35:57 +0800

    完成string相关所有命令

--- core/db/string.go
+++ core/db/string.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/HwHgoo/Gredis/core/protocol"
 	"github.com/HwHgoo/Gredis/global"
+	"github.com/HwHgoo/Gredis/utils"
 	"github.com/HwHgoo/Gredis/utils/pool"
 )
 
@@ -24,12 +25,6 @@ func (db *Database) getAsString(key string) ([]byte, protocol.RedisErrorMessage)
 
 	return s, nil
 }
-
-const (
-	upsert = iota // default set
-	insert        // nx
-	update        // xx
-)
 
 const (
 	flag_no_flag = 1 << iota
@@ -58,7 +53,7 @@ const (
  * If nil is returned, a duration value is also returned if needed.
  * The unit of duraton is always in milliseconds.
  */
-func parseExtendedStringArgumentOrReply(args [][]byte, flags *int, command_type int) (protocol.RedisErrorMessage, time.Duration) {
+func parseExtendedStringArgumentOrReply(args CommandParams, flags *int, command_type int) (protocol.RedisErrorMessage, time.Duration) {
 	d := time.Duration(0)
 	unixms := time.Now().UnixMilli()
 	for i := 0; i < len(args); i++ {
@@ -157,7 +152,7 @@ func parseExtendedStringArgumentOrReply(args [][]byte, flags *int, command_type 
 
 /************************************* GET ************************************/
 
-func getCommand(db *Database, args [][]byte) protocol.RedisMessage {
+func getCommand(db *Database, args CommandParams) protocol.RedisMessage {
 	key := string(args[0])
 	bs, err := db.getAsString(key)
 	if err != nil {
@@ -172,7 +167,7 @@ func getCommand(db *Database, args [][]byte) protocol.RedisMessage {
 }
 
 // GETDEL get the value of key and delete the key
-func getdelCommand(db *Database, args [][]byte) protocol.RedisMessage {
+func getdelCommand(db *Database, args CommandParams) protocol.RedisMessage {
 	key := string(args[0])
 	s, msg := db.getAsString(key)
 	if msg != nil {
@@ -188,7 +183,7 @@ func getdelCommand(db *Database, args [][]byte) protocol.RedisMessage {
 }
 
 // GETEX get the value of key and optionally set its expiration time
-func getexCommand(db *Database, args [][]byte) protocol.RedisMessage {
+func getexCommand(db *Database, args CommandParams) protocol.RedisMessage {
 	key := string(args[0])
 	flag := flag_no_flag
 	err, ttl := parseExtendedStringArgumentOrReply(args[1:], &flag, command_get)
@@ -216,7 +211,7 @@ func getexCommand(db *Database, args [][]byte) protocol.RedisMessage {
 }
 
 // GETRANGE get a substring of the string stored at key
-func getrangeCommand(db *Database, args [][]byte) protocol.RedisMessage {
+func getrangeCommand(db *Database, args CommandParams) protocol.RedisMessage {
 	key := string(args[0])
 	start, err := strconv.ParseInt(string(args[1]), 10, 64)
 	if err != nil {
@@ -243,6 +238,29 @@ func getrangeCommand(db *Database, args [][]byte) protocol.RedisMessage {
 	return protocol.MakeBulkString(b[start : end+1])
 }
 
+func mgetCommand(db *Database, args CommandParams) protocol.RedisMessage {
+	keys := make([]string, 0, len(args))
+	for _, arg := range args {
+		keys = append(keys, strings.ToLower(string(arg)))
+	}
+
+	values := make([]protocol.RedisMessage, 0, len(args))
+	for _, key := range keys {
+		v, err := db.getAsString(key)
+		if err != nil {
+			return err
+		}
+
+		if v == nil {
+			values = append(values, &protocol.RedisNil)
+		} else {
+			values = append(values, protocol.MakeBulkString(v))
+		}
+	}
+
+	return protocol.MakeArray(values)
+}
+
 // GETSET is deprecated, use SET with GET option instead.
 
 /************************************* INCR/DECR ************************************/
@@ -265,12 +283,12 @@ func incrdecrGeneric(db *Database, key string, delta int64) protocol.RedisMessag
 	return protocol.MakeInteger(n)
 }
 
-func incrCommand(db *Database, args [][]byte) protocol.RedisMessage {
+func incrCommand(db *Database, args CommandParams) protocol.RedisMessage {
 	key := string(args[0])
 	return incrdecrGeneric(db, key, 1)
 }
 
-func incrbyCommand(db *Database, args [][]byte) protocol.RedisMessage {
+func incrbyCommand(db *Database, args CommandParams) protocol.RedisMessage {
 	key := string(args[0])
 	delta, err := strconv.ParseInt(string(args[1]), 10, 64)
 	if err != nil {
@@ -280,12 +298,12 @@ func incrbyCommand(db *Database, args [][]byte) protocol.RedisMessage {
 	return incrdecrGeneric(db, key, delta)
 }
 
-func decrCommand(db *Database, args [][]byte) protocol.RedisMessage {
+func decrCommand(db *Database, args CommandParams) protocol.RedisMessage {
 	key := string(args[0])
 	return incrdecrGeneric(db, key, -1)
 }
 
-func decrbyCommand(db *Database, args [][]byte) protocol.RedisMessage {
+func decrbyCommand(db *Database, args CommandParams) protocol.RedisMessage {
 	key := string(args[0])
 	delta, err := strconv.ParseInt(string(args[1]), 10, 64)
 	if err != nil {
@@ -300,7 +318,7 @@ var bigfloats = pool.MakePool(1024, func() *big.Float {
 })
 
 // NOT as sensiable as redis
-func incrbyfloatCommand(db *Database, args [][]byte) protocol.RedisMessage {
+func incrbyfloatCommand(db *Database, args CommandParams) protocol.RedisMessage {
 	key := string(args[0])
 	var err error
 	delta := bigfloats.Get()
@@ -329,7 +347,7 @@ func incrbyfloatCommand(db *Database, args [][]byte) protocol.RedisMessage {
 
 /************************************* SET ************************************/
 
-func setCommand(db *Database, args [][]byte) protocol.RedisMessage {
+func setCommand(db *Database, args CommandParams) protocol.RedisMessage {
 	key := string(args[0])
 	value := args[1]
 	flag := flag_no_flag
@@ -364,7 +382,61 @@ func setCommand(db *Database, args [][]byte) protocol.RedisMessage {
 	return &protocol.RedisNil
 }
 
-func delCommand(db *Database, args [][]byte) protocol.RedisMessage {
+// Deprecated
+func setnxCommand(db *Database, args CommandParams) protocol.RedisMessage {
+	return protocol.MakeBulkString([]byte("Deprecated. Use SET with NX option instead."))
+}
+
+// Deprecated
+func setexCommand(db *Database, args CommandParams) protocol.RedisMessage {
+	return protocol.MakeBulkString([]byte("Deprecated. Use SET with EX option instead."))
+}
+
+// Deprecated
+func psetexCommand(db *Database, args CommandParams) protocol.RedisMessage {
+	return protocol.MakeBulkString([]byte("Deprecated. Use SET with PX option instead."))
+}
+
+func setrangeCommand(db *Database, args CommandParams) protocol.RedisMessage {
+	key := string(args[0])
+	offset, err := strconv.ParseInt(string(args[1]), 10, 64)
+	if err != nil {
+		return &protocol.InvalidIntegerError
+	}
+	if offset < 0 {
+		return &protocol.OffsetOutofRangeError
+	}
+
+	prefix, msg := db.getAsString(key)
+	if msg != nil {
+		return msg
+	}
+	suffix := args[2]
+	if len(suffix) == 0 {
+		return protocol.MakeInteger(int64(len(prefix)))
+	}
+
+	newval := make([]byte, offset+int64(len(suffix)))
+	copy(newval, prefix)
+	copy(newval[offset:], suffix)
+	db.Set(key, newval)
+	return protocol.MakeInteger(int64(len(newval)))
+}
+
+func msetCommand(db *Database, args CommandParams) protocol.RedisMessage {
+	if len(args)%2 != 0 {
+		return protocol.MakeWrongNumberOfArgError("mset")
+	}
+
+	for i := 0; i < len(args); i += 2 {
+		key := string(args[i])
+		db.Set(key, args[i+1])
+	}
+
+	return &protocol.RedisOk
+}
+
+func delCommand(db *Database, args CommandParams) protocol.RedisMessage {
 	keys := make([]string, 0, len(args))
 	for _, arg := range args {
 		keys = append(keys, string(arg))
@@ -380,7 +452,7 @@ func delCommand(db *Database, args [][]byte) protocol.RedisMessage {
 
 /************************************* OTHER ************************************/
 
-func appendCommand(db *Database, args [][]byte) protocol.RedisMessage {
+func appendCommand(db *Database, args CommandParams) protocol.RedisMessage {
 	key := string(args[0])
 	suffix := args[1]
 
@@ -399,19 +471,176 @@ func appendCommand(db *Database, args [][]byte) protocol.RedisMessage {
 	return protocol.MakeInteger(int64(len(newval)))
 }
 
+func strlenCommand(db *Database, args CommandParams) protocol.RedisMessage {
+	key := string(args[0])
+	v, msg := db.getAsString(key)
+	if msg != nil {
+		return msg
+	}
+
+	return protocol.MakeInteger(int64(len(v)))
+}
+
+// Deprecated
+func substrCommand(db *Database, args CommandParams) protocol.RedisMessage {
+	return protocol.MakeBulkString([]byte("Deprecated. Use GETRANGE instead."))
+}
+
+func lcsCommand(db *Database, args CommandParams) protocol.RedisMessage {
+	key1, key2 := string(args[0]), string(args[1])
+	idx, withmatchlen := false, false
+	minmatchlen := int64(0)
+	for i := 2; i < len(args); i++ {
+		arg := strings.ToLower(string(args[i]))
+		if arg == "idx" {
+			idx = true
+		} else if arg == "withmatchlen" {
+			withmatchlen = true
+		} else if arg == "minmatchlen" {
+			if i+1 >= len(args) {
+				return &protocol.SyntaxError
+			}
+			minlen, err := strconv.ParseInt(string(args[i+1]), 10, 64)
+			if err != nil {
+				return &protocol.InvalidIntegerError
+			}
+			minmatchlen = minlen
+		} else {
+			return &protocol.SyntaxError
+		}
+	}
+
+	var s1, s2 []byte
+	var msg protocol.RedisErrorMessage
+	s1, msg = db.getAsString(key1)
+	if msg != nil {
+		return msg
+	}
+
+	nomatch := protocol.MakeArray([]protocol.RedisMessage{
+		protocol.MakeBulkString([]byte("matches")),
+		protocol.MakeArray(nil),
+		protocol.MakeBulkString([]byte("len")),
+		protocol.MakeInteger(0),
+	})
+	if s1 == nil {
+		return nomatch
+	}
+
+	s2, msg = db.getAsString(key2)
+	if msg != nil {
+		return msg
+	}
+	if s2 == nil {
+		return nomatch
+	}
+
+	dp := make([][]int, len(s1)+1)
+	for i := range dp {
+		dp[i] = make([]int, len(s2)+1)
+	}
+
+	for i := 1; i <= len(s2); i++ {
+		for j := 1; j <= len(s1); j++ {
+			if s2[i-1] == s1[j-1] {
+				dp[j][i] = dp[j-1][i-1] + 1
+			} else {
+				dp[j][i] = max(dp[j-1][i], dp[j][i-1])
+			}
+		}
+	}
+
+	lcsLen := dp[len(s1)][len(s2)]
+	lcs := make([]byte, lcsLen)
+
+	index := lcsLen - 1
+	var s1idx, s2idx []int
+	if idx {
+		s1idx, s2idx = make([]int, lcsLen+1), make([]int, lcsLen+1)
+	}
+	for i, j := len(s1), len(s2); index >= 0 && i > 0 && j > 0; {
+		if dp[i][j] == dp[i-1][j-1]+1 && s2[j-1] == s1[i-1] {
+			lcs[index] = s1[i-1]
+			if idx {
+				s1idx[index], s2idx[index] = i-1, j-1
+			}
+			index, i, j = index-1, i-1, j-1
+		} else if dp[i][j] == dp[i-1][j] {
+			i--
+		} else {
+			j--
+		}
+	}
+
+	if !idx {
+		return protocol.MakeBulkString([]byte(lcs))
+	}
+
+	matches := make([]protocol.RedisMessage, 0)
+	s1idx[lcsLen], s2idx[lcsLen] = s1idx[lcsLen-1]+1, s2idx[lcsLen-1]+1
+	last := lcsLen
+	makematch := func(start, end int) protocol.RedisMessage {
+		elems := []protocol.RedisMessage{
+			protocol.MakeArray([]protocol.RedisMessage{
+				protocol.MakeInteger(int64(s1idx[start])),
+				protocol.MakeInteger(int64(s1idx[end])),
+			}),
+
+			protocol.MakeArray([]protocol.RedisMessage{
+				protocol.MakeInteger(int64(s2idx[start])),
+				protocol.MakeInteger(int64(s2idx[end])),
+			}),
+
+			utils.TerneryOp(withmatchlen, protocol.MakeInteger(int64(end-start+1)), nil),
+		}
+		if !withmatchlen {
+			elems = elems[:len(elems)-1]
+		}
+
+		return protocol.MakeArray(elems)
+	}
+	for i := lcsLen - 1; i >= 0; i-- {
+		if s1idx[i]+1 == s1idx[i+1] && s2idx[i]+1 == s2idx[i+1] {
+			continue
+		}
+
+		length := last - i
+		if length >= int(minmatchlen) {
+			matches = append(matches, makematch(i+1, last-1))
+		}
+		last = i + 1
+	}
+
+	if last != 0 {
+		matches = append(matches, makematch(0, last-1))
+	}
+
+	return protocol.MakeArray([]protocol.RedisMessage{
+		protocol.MakeBulkString([]byte("matches")),
+		protocol.MakeArray(matches),
+		protocol.MakeBulkString([]byte("len")),
+		protocol.MakeInteger(int64(lcsLen)),
+	})
+}
+
 func init() {
-	RegisterCommand("get", 2, getCommand)
 	RegisterCommand("set", -3, setCommand)
+	RegisterCommand("mset", -3, msetCommand)
+	RegisterCommand("setrange", 4, setrangeCommand)
 	RegisterCommand("del", -2, delCommand)
+	RegisterCommand("get", 2, getCommand)
 	RegisterCommand("getdel", 2, getdelCommand)
 	RegisterCommand("getex", -2, getexCommand)
 	RegisterCommand("getrange", 4, getrangeCommand)
+	RegisterCommand("mget", -2, mgetCommand)
 	RegisterCommand("incr", 2, incrCommand)
 	RegisterCommand("incrby", 3, incrbyCommand)
 	RegisterCommand("decr", 2, decrCommand)
 	RegisterCommand("decrby", 3, decrbyCommand)
 	RegisterCommand("incrbyfloat", 3, incrbyfloatCommand)
 	RegisterCommand("append", 3, appendCommand)
+	RegisterCommand("lcs", -3, lcsCommand)
+	RegisterCommand("strlen", 2, strlenCommand)
 }
 
 // withFlags checks if the given flag contains some of the given flags.
